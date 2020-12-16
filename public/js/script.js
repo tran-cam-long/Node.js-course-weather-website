0d2f376bfdefc6e56467cc44742ac80153692b74
tran-cam-long <camlong.tran0702@gmail.com>
2020-12-16 11:44:49 +0700

    Setup app for Heroku

--- public/js/script.js
+++ public/js/script.js
@@ -30,7 +30,7 @@ weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const address = search.value;
-  fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
+  fetch(`/weather?address=${address}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         document
